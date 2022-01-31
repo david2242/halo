5957695eb7164c4f77a9bf1e4706a7b46dacdbc8
David_git <plusz.egy.cim@gmail.com>
2022-02-01 00:27:53 +0100

    meggyógyult a pushing-data

--- tools/pushing-data.js
+++ tools/pushing-data.js
@@ -7,7 +7,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { addDoc, collection } from "firebase/firestore";
 
-import * as data from "data.json";
+import * as data from "./data.json";
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "c:/doku/own/halo/node_modules/firebase/app";
 // import { getDoc, getDocs, deleteDoc, doc, collection, addDoc, updateDoc, setDoc, query, getFirestore } from 'c:/doku/own/halo/node_modules/firebase/app';
@@ -26,21 +26,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBogLfVcHGpisc-2iDqHMFjvyr1MIhMigo",
-//     authDomain: "halo-60249.firebaseapp.com",
-//     projectId: "halo-60249",
-//     storageBucket: "halo-60249.appspot.com",
-//     messagingSenderId: "124850108802",
-//     appId: "1:124850108802:web:2847891383bfc9fa92f79b"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore Database
 const db = getFirestore();
 
 console.log('Firebase initialized');
